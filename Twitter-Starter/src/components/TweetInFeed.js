6935e8d949fc9d66f43079c37e296d189e3a3d47
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 18:01:45 +0200

    $0013: [HomePage]: [TwitterFeed]: add interactions to tweets in feed (like, tip, comment)

--- Twitter-Starter/src/components/TweetInFeed.js
+++ Twitter-Starter/src/components/TweetInFeed.js
@@ -25,9 +25,46 @@ const TweetInFeed = () => {
             <img src={golf} className="tweetImg"></img>
           </div>
 
-
+          <div className="interactions">
+           <div className="interactionNums">
+             <Icon fill="#6f6f6f" size={20} svg="messageCircle" />
+           </div>
+           <div className="interactionNums">
+             <Icon fill="#6f6f6f" size={20} svg="star" />
+             12
+           </div>
+           <div className="interactionNums">
+             <Icon fill="#6f6f6f" size={20} svg="matic" />
+           </div>
+          </div>
         </div>
       </div>
+
+       <div className="feedTweet">
+       <img src={defaultImgs[0]} className="profilePic"></img>
+       <div className="completeTweet">
+         <div className="who">
+           Bob
+           <div className="accWhen">0x42..314 · 1h</div>
+         </div>
+         <div className="tweetContent">
+           Love spending time on the water 🌊🌅
+           <img src={canoe} className="tweetImg"></img>
+         </div>
+         <div className="interactions">
+           <div className="interactionNums">
+             <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+           </div>
+           <div className="interactionNums">
+             <Icon fill="#3f3f3f" size={20} svg="star" />
+             12
+           </div>
+           <div className="interactionNums">
+             <Icon fill="#3f3f3f" size={20} svg="matic" />
+           </div>
+         </div>
+       </div>
+     </div>
     </>
   );
 };
