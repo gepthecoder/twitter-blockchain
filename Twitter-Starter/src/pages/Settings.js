bb5d673f5af4babdeae5c4aa04ee756a2270c528
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 20:03:35 +0200

    $0018: [SettingsPage]: choose profile NFT, update banner + save option (frontend)

--- Twitter-Starter/src/pages/Settings.js
+++ Twitter-Starter/src/pages/Settings.js
@@ -3,9 +3,34 @@ import { Link } from "react-router-dom";
 import './Settings.css';
 
 import { Input } from "web3uikit";
+import { useState, useRef, useEffect } from "react";
+
+import pfp1 from "../images/pfp1.png";
+import pfp2 from "../images/pfp2.png";
+import pfp3 from "../images/pfp3.png";
+import pfp4 from "../images/pfp4.png";
+import pfp5 from "../images/pfp5.png";
+import { defaultImgs } from "../defaultimgs";
 
 const Settings = () => {
-  
+
+  const pfps = [pfp1, pfp2, pfp3, pfp4, pfp5];
+  const [selectedPFP, setSelectedPFP] = useState();
+  const inputFile = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(defaultImgs[1]);
+  const [theFile, setTheFile] = useState();
+  const [username, setUsername] = useState();
+  const [bio, setBio] = useState();
+
+  const onBannerClick = () => {
+   inputFile.current.click();
+  };
+
+  const changeHandler = (event) => {
+   const img = event.target.files[0];
+   setTheFile(img);
+   setSelectedFile(URL.createObjectURL(img));
+  };
 
   return (
     <>
@@ -26,6 +51,45 @@ const Settings = () => {
         labelBgColor="#141d26"
         /*onChange={(e)=> setBio(e.target.value)}*/
       />
+
+      <div className="pfp">
+        Profile Image (Your NFTs)
+        <div className="pfpOptions">
+          {pfps.map((e,i) => {
+             return(
+               <>
+               <img
+               src={e}
+               className={ selectedPFP === e ? "pfpOptionSelected" : "pfpOption" }
+               onClick={() => {setSelectedPFP(pfps[i]);}}
+               ></img>
+               </>
+             )
+           })}
+        </div>
+      </div>
+
+      <div className="pfp">
+         Profile Banner
+         <div className="pfpOptions">
+           <img
+             src={selectedFile}
+             onClick={onBannerClick}
+             className="banner"
+           ></img>
+           <input
+             type="file"
+             name="file"
+             ref={inputFile}
+             onChange={changeHandler}
+             style={{ display: "none" }}
+           />
+         </div>
+       </div>
+       
+      <div className="save" /* onClick={() => saveEdits()} */>
+         Save
+      </div>
 
     </div>
 
