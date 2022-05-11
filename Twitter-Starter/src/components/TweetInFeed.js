35068f7eff25720f5e256f373a3c37f30f4c4a6b
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-12 00:15:47 +0200

    $0026: [Moralis]: [FetchingTweets]: retrive all published tweets and display them in home

--- Twitter-Starter/src/components/TweetInFeed.js
+++ Twitter-Starter/src/components/TweetInFeed.js
@@ -6,65 +6,80 @@ import canoe from "../images/canoe.png";
 import { defaultImgs } from "../defaultimgs";
 import { Icon } from "web3uikit";
 
-const TweetInFeed = () => {
-  
+import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+
+const TweetInFeed = ({profile}) => {
+
+  //store all tweets on moralis db
+  const [tweetArr, setTweetArr] = useState();
+  // account to get user wallet address for filtering
+  const { Moralis, account } = useMoralis();
+
+  useEffect(() => {
+   async function getTweets() {
+     try {
+       const Tweets = Moralis.Object.extend("Tweets");
+       const query = new Moralis.Query(Tweets);
+       if (profile) {
+         query.equalTo("tweeterAcc", account);
+       }
+       const results = await query.find();
+ 
+       setTweetArr(results);
+       console.log(results);
+     } catch (error) {
+       console.error(error);
+     }
+   }
+   getTweets();
+}, [profile]);
 
   return (
     <>
-      <div className="feedTweet">
-        <img src={defaultImgs[0]} className="profilePic"></img>
-        <div className="completeTweet">
-
-          <div className="who">
-            Gasper
-            <div className="accWhen">0x42..314 · 1h</div>
-          </div>
-
-           <div className="tweetContent">
-            Nice Day On The Sun Today! Played golf with my friends
-            <img src={golf} className="tweetImg"></img>
-          </div>
-
-          <div className="interactions">
-           <div className="interactionNums">
-             <Icon fill="#6f6f6f" size={20} svg="messageCircle" />
+    {tweetArr?.map((e) => {
+       return (
+         <>
+           <div className="feedTweet">
+             <img src={e.attributes.tweeterPfp ? e.attributes.tweeterPfp : defaultImgs[0]} className="profilePic"></img>
+             <div className="completeTweet">
+               <div className="who">
+               {e.attributes.tweeterUserName.slice(0, 6)}
+                 <div className="accWhen">{
+                       `${e.attributes.tweeterAcc.slice(0, 4)}...${e.attributes.tweeterAcc.slice(38)} ·
+                       ${e.attributes.createdAt.toLocaleString('en-us', { month: 'short' })} 
+                       ${e.attributes.createdAt.toLocaleString('en-us', { day: 'numeric' })}
+                       ` 
+                     }
+                     </div>
+               </div>
+               <div className="tweetContent">
+               {e.attributes.tweetTxt}
+               {e.attributes.tweetImg && (
+                       <img
+                         src={e.attributes.tweetImg}
+                         className="tweetImg"
+                       ></img>
+                     )}
+               </div>
+               <div className="interactions">
+                 <div className="interactionNums">
+                   <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+                 </div>
+                 <div className="interactionNums">
+                   <Icon fill="#3f3f3f" size={20} svg="star" />
+                   12
+                 </div>
+                 <div className="interactionNums">
+                   <Icon fill="#3f3f3f" size={20} svg="matic" />
+                 </div>
+               </div>
+             </div>
            </div>
-           <div className="interactionNums">
-             <Icon fill="#6f6f6f" size={20} svg="star" />
-             12
-           </div>
-           <div className="interactionNums">
-             <Icon fill="#6f6f6f" size={20} svg="matic" />
-           </div>
-          </div>
-        </div>
-      </div>
-
-       <div className="feedTweet">
-       <img src={defaultImgs[0]} className="profilePic"></img>
-       <div className="completeTweet">
-         <div className="who">
-           Gasper
-           <div className="accWhen">0x42..314 · 1h</div>
-         </div>
-         <div className="tweetContent">
-           Love spending time on the water 🌊🌅
-           <img src={canoe} className="tweetImg"></img>
-         </div>
-         <div className="interactions">
-           <div className="interactionNums">
-             <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
-           </div>
-           <div className="interactionNums">
-             <Icon fill="#3f3f3f" size={20} svg="star" />
-             12
-           </div>
-           <div className="interactionNums">
-             <Icon fill="#3f3f3f" size={20} svg="matic" />
-           </div>
-         </div>
-       </div>
-     </div>
+         </>
+       );
+     }).reverse()}
+    
     </>
   );
 };
