457af978e49648f05bf45a778f41ac4f954f2523
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 13:37:00 +0200

    $0009: [HomePage]: [TweetBox]: attach image to tweet option part1

--- Twitter-Starter/src/pages/Home.js
+++ Twitter-Starter/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { defaultImgs } from "../defaultimgs";
-import { TextArea } from "web3uikit";
+import { TextArea, Icon } from "web3uikit";
 
 const Home = () => {
   return (
@@ -20,6 +20,16 @@ const Home = () => {
             type="text"
             width="95%"
           ></TextArea>
+
+          <div className="imgOrTweet">
+            <div className="imgDiv">
+              <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
+            </div>
+
+             <div className="tweetOptions">
+            
+            </div>
+          </div>
         </div>
       </div>
     </div>
