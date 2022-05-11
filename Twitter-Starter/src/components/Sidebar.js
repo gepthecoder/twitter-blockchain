b6bd135b05da031dc2705753dd6e6465c943db15
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 13:10:26 +0200

    $0004: [AppLayout]: [SideBars]: wrap menu items with links

--- Twitter-Starter/src/components/Sidebar.js
+++ Twitter-Starter/src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Sidebar.css';
 import { Icon } from "web3uikit";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   
@@ -13,20 +14,26 @@ const Sidebar = () => {
           <Icon fill="#ffffff" size={33} svg="twitter" />
         </div>
 
-        <div className="menuItems">
-          <Icon fill="#ffffff" size={33} svg="list" />
-          Home
-        </div>
+        <Link to="/" className="link">
+          <div className="menuItems">
+            <Icon fill="#ffffff" size={33} svg="list" />
+            Home
+          </div>
+        </Link>
 
-         <div className="menuItems">
-          <Icon fill="#ffffff" size={33} svg="user" />
-          Profile
-        </div>
+        <Link to="/profile" className="link">
+          <div className="menuItems">
+            <Icon fill="#ffffff" size={33} svg="user" />
+            Profile
+          </div>
+        </Link>
 
-         <div className="menuItems">
-          <Icon fill="#ffffff" size={33} svg="cog" />
-          Settings
-        </div>
+        <Link to="/settings" className="link">
+          <div className="menuItems">
+            <Icon fill="#ffffff" size={33} svg="cog" />
+            Settings
+          </div>
+        </Link>
 
       </div>
     </div>
