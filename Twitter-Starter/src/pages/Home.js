190befa4ac48b74ecd33df453d8a90118dfd8bde
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 13:49:42 +0200

    $0011: [HomePage]: [TweetBox]: attach image to tweet option - functionality part2

--- Twitter-Starter/src/pages/Home.js
+++ Twitter-Starter/src/pages/Home.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./Home.css";
 import { defaultImgs } from "../defaultimgs";
 import { TextArea, Icon } from "web3uikit";
+import { useState, useRef } from "react";
 
 const Home = () => {
+
+  const inputFile = useRef(null);
+  const [selectedFile, setSelectedFile] = useState();
+
+  const [theFile, setTheFile] = useState();
+  const [tweet, setTweet] = useState();
+
+  const onImageClick = () => {
+   inputFile.current.click();
+  };
+
+  // whenever the event happens in the file selector we get the file we selected and save it to a img const
+  // afterwards we set the selected file to a temp object url so we can display it
+  const changeHandler = (event) => {
+    const img = event.target.files[0];
+    setTheFile(img);
+    setSelectedFile(URL.createObjectURL(img));
+  };
+
   return (
     <>
     <div className="mainContent">
@@ -20,9 +40,18 @@ const Home = () => {
             type="text"
             width="95%"
           ></TextArea>
-
+          {selectedFile && (
+             <img src={selectedFile} className="tweetImg"></img>
+           )}
           <div className="imgOrTweet">
-            <div className="imgDiv">
+            <div className="imgDiv" onClick={onImageClick}>
+              <input
+                type="file"
+                 name="file"
+                 ref={inputFile}
+                 onChange={changeHandler}
+                 style={{ display: "none"}}
+              />
               <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
             </div>
 
