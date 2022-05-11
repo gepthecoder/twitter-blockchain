a17efe02d4e5b5cd7e193b39a6cb9f92e7b0f2a9
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 22:09:45 +0200

    $0020: [Moralis]: user authentication - login + sign out

--- Twitter-Starter/src/App.js
+++ Twitter-Starter/src/App.js
@@ -7,13 +7,30 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 
+import { useMoralis } from "react-moralis";
+import { ConnectButton, Icon } from "web3uikit";
+
 const App = () => {
 
+  const { isAuthenticated, Moralis } = useMoralis();
+
+  // conditionally render this pages only if the user is authenticated otherwise show login
   return (
     <>
+    { isAuthenticated ? (
       <div className="page">
         <div className="sideBar">
           <Sidebar />
+          <div
+          className="logout"
+          onClick={() => {
+            Moralis.User.logOut().then(() => {
+              window.location.reload();
+            });
+          }}
+          >
+            Logout
+           </div>
         </div>
           <div className="mainWindow">
             <Routes>
@@ -26,6 +43,12 @@ const App = () => {
           <Rightbar />
         </div>
       </div>
+      ) : (
+        <div className="loginPage">
+         <Icon fill="#ffffff" size={40} svg="twitter" />
+         <ConnectButton />
+        </div>
+      )}
     </>
   );
 };
