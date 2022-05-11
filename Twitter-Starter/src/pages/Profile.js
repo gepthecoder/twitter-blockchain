7f347cc8e6aff4609b6d25bfbf3ebc98acd7444a
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-11 19:30:37 +0200

    $0015: [ProfilePage]: banner and profile picture

--- Twitter-Starter/src/pages/Profile.js
+++ Twitter-Starter/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Profile.css';
+import { defaultImgs } from "../defaultimgs";
 
 
 const Profile = () => {
@@ -9,7 +10,10 @@ const Profile = () => {
   return (
     <>
       <div className="pageIdentify">Profile</div>
-
+      <img className="profileBanner" src={defaultImgs[1]}></img>
+      <div className="pfpContainer">
+         <img className="profilePFP" src={defaultImgs[0]}></img>
+      </div>
     </>
   );
 };
